e611fb053f1c229d695d2a8da54c8bae0a218127
leDonChung <ledonchung12a2@gmail.com>
2025-04-07 16:27:28 +0700

    fixed choose tab navigation

--- alo-mobile-frontend/app/presentation/pages/inapp/AccountScreen.js
+++ alo-mobile-frontend/app/presentation/pages/inapp/AccountScreen.js
@@ -11,7 +11,7 @@ export const AccountScreen = ({ navigation }) => {
     const dispatch = useDispatch();
     const userLogin = useSelector(state => state.user.userLogin);
     return (
-        <SafeAreaView style={GlobalStyles.container}>
+        <View style={GlobalStyles.container}>
             <View style={{ height: 300, paddingHorizontal: 15 }}>
                 <TouchableOpacity style={{ flex: 1, alignItems: 'center', flexDirection: 'row', paddingVertical: 40, borderBottomColor: '#b0b3ba', borderBottomWidth: 1 }}>
                     <Image source={{ uri: userLogin?.avatarLink || "https://my-alo-bucket.s3.amazonaws.com/1742401840267-OIP%20%282%29.jpg" }} style={{ width: 60, height: 60, borderRadius: 50 }} />
@@ -54,6 +54,6 @@ export const AccountScreen = ({ navigation }) => {
                     <IconMaterial name="keyboard-arrow-right" size={24} color={"#000"} />
                 </TouchableOpacity>
             </View>
-        </SafeAreaView>
+        </View>
     )
 }
