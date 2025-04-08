f916e3cceb6eade324fc0664cc242d487252a561
Kiệt Trần <trantuankiet5980@gmail.com>
2025-04-09 00:44:36 +0700

    update style error

--- alo-mobile-frontend/app/presentation/pages/auth/ForgetPassword.js
+++ alo-mobile-frontend/app/presentation/pages/auth/ForgetPassword.js
@@ -229,6 +229,8 @@ const styles = StyleSheet.create({
     color: "red",
     fontSize: 14,
     marginBottom: 10,
+    alignSelf: "flex-start",
+    marginLeft: "10%",
   },
   button: {
     width: "80%",
