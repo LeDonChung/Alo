a2a06cb0ebfeeb809eb1e7b4672106ce04d4e640
leDonChung <ledonchung12a2@gmail.com>
2025-04-25 16:06:10 +0700

    change properties model

--- alo-nodejs-backend/src/models/User.js
+++ alo-nodejs-backend/src/models/User.js
@@ -3,14 +3,12 @@ class User {
         this.id = id;
         this.fullName = fullName;
         this.gender = gender;
-        this.email = email;
         this.birthDay = birthDay;
-        this.status = status;
         this.avatarLink = avatarLink;
         this.backgroundLink = backgroundLink;
         this.accountId = accountId;
         this.createdAt = createdAt;
-        this.lastLogin = lastLogin;
+        this.lastLogout = lastLogout;
     }
 }
 
