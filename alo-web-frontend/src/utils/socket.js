54bd283793d57d6c1f5ffd6aed13e9062d5132fa
leDonChung <ledonchung12a2@gmail.com>
2025-05-16 14:42:25 +0700

    config cors backend

--- alo-web-frontend/src/utils/socket.js
+++ alo-web-frontend/src/utils/socket.js
@@ -1,7 +1,16 @@
 import { io } from 'socket.io-client';
 
-const socket = io(`${process.env.REACT_APP_SOCKET_URL}`, {
-    withCredentials: true,
+const socket = io(process.env.REACT_APP_SOCKET_URL, {
+  withCredentials: true,
+  transports: ['websocket'],
+});
+
+socket.on("connect_error", (err) => {
+  console.error("Socket connect error:", err);
+});
+
+socket.on("disconnect", (reason) => {
+  console.log("Socket disconnected:", reason);
 });
 
 export default socket;
